50d701e02b84676c035d6015e7c89e481c22bc36
User <user@hackerrank.workspace>
2024-01-18 07:40:36 +0000

    Add initial repository
    Commit by HackerRank

---
+++ RoomService.WebAPI/PasswordCheckerMiddleware.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace RoomService.WebAPI
+{
+    public class PasswordCheckerMiddleware
+    {
+        public PasswordCheckerMiddleware(RequestDelegate next)
+        {
+
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+        }
+    }
+}
